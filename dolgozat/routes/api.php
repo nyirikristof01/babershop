b07cc1a1ce91fdb4966fdc2264f6643ec22d3290
nyiri.kristof <nyiri.kristof@icloud.com>
2025-02-28 09:20:12 +0100

    Update api.php

--- dolgozat/routes/api.php
+++ dolgozat/routes/api.php
@@ -4,14 +4,14 @@ use App\Http\Controllers\BarberController;
 use App\Http\Controllers\AppointmentController;
 use Illuminate\Support\Facades\Route;
 
-// Barber
+// barber
 Route::get('barbers', [BarberController::class, 'index']);
 Route::get('barbers/{id}', [BarberController::class, 'show']);
 Route::post('barbers', [BarberController::class, 'store']);
 Route::put('barbers/{id}', [BarberController::class, 'update']);
 Route::delete('barbers/{id}', [BarberController::class, 'destroy']);
 
-// Appointment
+// appointment
 Route::get('appointments', [AppointmentController::class, 'index']);
 Route::get('appointments/{id}', [AppointmentController::class, 'show']);
 Route::post('appointments', [AppointmentController::class, 'store']);
